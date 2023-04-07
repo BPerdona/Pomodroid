0f0f2f97edbfb142c2562602a15f974845d36fcd
Bruno Perdona <bruno15perdona@gmail.com>
2023-04-07 14:41:08 -0300

    Request Permissions

--- app/src/main/java/com/brunoperdona/pomodroid/MainActivity.kt
+++ app/src/main/java/com/brunoperdona/pomodroid/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.brunoperdona.pomodroid
 
-import android.graphics.drawable.ColorDrawable
+import android.Manifest
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.content.res.AppCompatResources
 import com.brunoperdona.pomodroid.databinding.ActivityMainBinding
 
@@ -12,11 +15,26 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            requestPermissions(Manifest.permission.POST_NOTIFICATIONS)
+        }
+
         supportActionBar?.setBackgroundDrawable(
             AppCompatResources.getDrawable(applicationContext, R.color.strong_pink))
         supportActionBar?.elevation = 10f
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+    }
+
+    private fun requestPermissions(vararg permissions: String){
+        val requestPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ){ result ->
+            result.entries.forEach{
+                Log.d("Main", "${it.key} ${it.value}")
+            }
+        }
+        requestPermissionLauncher.launch(permissions.asList().toTypedArray())
     }
 }
