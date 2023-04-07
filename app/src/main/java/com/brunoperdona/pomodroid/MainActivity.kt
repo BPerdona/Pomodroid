4a30256986adbf7159a8619ae7a0dfa560fdf138
Bruno Perdona <bruno15perdona@gmail.com>
2023-04-07 13:52:53 -0300

    Impl Gradle Version Catalog

--- app/src/main/java/com/brunoperdona/pomodroid/MainActivity.kt
+++ app/src/main/java/com/brunoperdona/pomodroid/MainActivity.kt
@@ -8,14 +8,17 @@ import com.brunoperdona.pomodroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var mainBinding: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.setBackgroundDrawable(
-            AppCompatResources.getDrawable(applicationContext, R.color.strong_pink)
-        )
+            AppCompatResources.getDrawable(applicationContext, R.color.strong_pink))
         supportActionBar?.elevation = 10f
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        //binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
